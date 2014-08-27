a05ae898947bbca733bbf414bdd50e6b632b9cb7
cw2359540 <cwalsh5@student.rcc.edu>
2014-08-27 09:24:31 -0700

    Signed-off-by: cw2359540 <cwalsh5@student.rcc.edu>

---
+++ VariableStrings/VariableStrings.php
@@ -0,0 +1,30 @@
+<?php
+	/*
+    	Cheryllynn Walsh
+        August 27th, 2014
+        Purpose: Comments/Variable/
+    */
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Strings and Variables</title>
+</head>
+
+<body>
+	<?php
+		//Declare Variables
+		$hours=40;//Worked 40 hours
+		$payRate=9;//$'s/hour
+		//Calculate the paycheck
+		$grossPay=$hors*$payRate;
+		//Output the result
+		echo "<p>Hpurs worked = $hours (hrs)</P>";
+		echo '<p>Pay Ratte = $$payRate</p>';
+		echo "<p>Pay check = $".$grossPay.'</p>';
+			
+	
+	?>
+</body>
+</html>
